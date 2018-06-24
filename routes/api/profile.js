90ed297786b88d086e13c43c16282be22d500068
mnjit20 <mnjit.vrma@gmail.com>
2018-06-24 07:00:48 +0530

    delete api/profile/experience/exp_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -209,6 +209,7 @@ router.post('/experience', passport.authenticate('jwt', {
         description: req.body.description
       }
 
+
       //Add to experience array 
       profile.experience.unshift(newExp);
       profile.save().then(profile => res.json(profile));
@@ -255,11 +256,11 @@ router.post('/education', passport.authenticate('jwt', {
     })
 });
 
-// @route DELETE api/profile/experience/:exp_id
-// @desc  DELETE experience from profile
+// @route DELETE api/profile/education/:exp_id
+// @desc  DELETE education from profile
 // @access Private
 
-router.post('/experience/:exp_id', passport.authenticate('jwt', {
+router.delete('/education/:exp_id', passport.authenticate('jwt', {
   session: false
 }), (req, res) => {
 
@@ -267,11 +268,47 @@ router.post('/experience/:exp_id', passport.authenticate('jwt', {
     user: req.user.id
   }).then(profile => {
     //get remove index
-    const removeIndex = profile.experience;
+    const removeIndex = profile.education
+      .map(item => item.id).indexOf(req.params.exp_id);
+
+    //Splice out of Array
+    profile.education.splice(removeIndex, 1);
+
+    //Save
+    profile.save().then(profile => res.json(profile));
 
 
-  });
+  }).catch(err => res.status(404).json(err));
 
 
 });
+
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc  DELETE experience from profile
+// @access Private
+
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+
+  Profile.findOne({
+    user: req.user.id
+  }).then(profile => {
+    //get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id).indexOf(req.params.exp_id);
+
+    //Splice out of Array
+    profile.experience.splice(removeIndex, 1);
+
+    //Save
+    profile.save().then(profile => res.json(profile));
+
+
+  }).catch(err => res.status(404).json(err));
+
+
+});
+
 module.exports = router;
