eb26f75d52e29743724adeddfe1769adf574479c
mnjit20 <mnjit.vrma@gmail.com>
2018-07-08 19:42:55 +0530

    redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,11 +1,18 @@
+import axios from 'axios';
 import { TEST_DISPATCH } from './types';
 
-
 //Register user
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = userData => dispatch => {
+
+  axios.post('/api/users/register', newUser)
+    .then(res => console.log(res.data))
+    .catch(err => this.setState({ errors: err.response.data }));
+
+  console.log('new user submitted form', newUser);
+
+  // return {
+  //   type: TEST_DISPATCH,
+  //   payload: userData
+  // };
 
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
@@ -36,11 +35,6 @@ class Register extends Component {
     }
 
     this.props.registerUser(newUser);
-
-    // axios.post('/api/users/register', newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
-    // console.log('new user submitted form', newUser);
   }
 
   render() {
